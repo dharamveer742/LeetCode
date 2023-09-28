a726ad8e581e1e77813afc39e4f790204477c8a0
dharamveer742 <80776938+dharamveer742@users.noreply.github.com>
2023-09-28 07:04:43 +0530

    Time: 287 ms (5.06%), Space: 44.9 MB (15.00%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,32 @@
+// top-down approach
+
+// bottom up approach 
+import java.util.Arrays;
+class Solution {
+    public int jump(int[] nums) {
+        if(nums[0]==0){
+            return 0;
+        
+        }
+        int len= nums.length;
+        if(len==1){
+            return 0;
+        }
+        int[] dp= new int[len];
+        Arrays.fill(dp,Integer.MAX_VALUE);  // why are we filling maximum 
+        dp[0]=0;
+        for(int i=1;i<len;i++){
+            int min=Integer.MAX_VALUE;
+            for(int j=0;j<i;j++){
+                if(nums[j]+j>=i){
+                    min=Math.min(min,dp[j]);
+                }
+            }
+             
+            dp[i]=min+1;
+            
+           
+        }
+        return dp[len-1];
+    }
+}
