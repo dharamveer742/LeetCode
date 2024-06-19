f30133df86f9f188bdd50f32691a2bf6d26efd53
dharamveer742 <80776938+dharamveer742@users.noreply.github.com>
2024-06-19 16:40:32 +0530

    Time: 2 ms (72.94%), Space: 42.4 MB (31.77%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,12 +1,14 @@
 class Solution {
-     public int orangesRotting(int[][] grid) {
+    public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int cntFresh = 0;
         Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
+                    visited[i][j]=true;
                     q.offer(new int[]{i,j,0});
                 }
                 if(grid[i][j]==1){
@@ -28,9 +30,9 @@ class Solution {
             for(int i=0;i<4;i++){
                 int nrow = row+drow[i];
                 int ncol = col+dcol[i];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==1 ){
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==1 && visited[nrow][ncol]==false){
                     q.offer(new int[]{nrow,ncol,t+1});
-                    grid[nrow][ncol]=2;
+                    visited[nrow][ncol] = true; 
                     cnt++;
                 }
             }
