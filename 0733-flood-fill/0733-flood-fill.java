ab0a0a0266cd1a25c61a8f346589d53269aa90af
dharamveer742 <80776938+dharamveer742@users.noreply.github.com>
2024-06-19 15:52:36 +0530

    Time: 0 ms (100.00%), Space: 44.9 MB (36.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,28 +1,26 @@
 class Solution {
-      public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+   public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int iniColor = image[sr][sc];
-        //if(iniColor == color) return image;
-       // int[][] ans = image;
+        int[][] ans = image;
         int delRow[] = {-1, 0, +1, 0};
         int delCol[] = {0, +1, 0, -1}; 
-        dfs(sr, sc, image,iniColor,color, delRow, delCol); 
-        return image; 
+        dfs(sr, sc,ans,iniColor,color, delRow, delCol); 
+        return ans; 
         
     }
     
-  void dfs(int row,int col,int[][] image,int iniColor,int newColor,int delRow[], int delCol[]){
-       // ans[row][col] = newColor;
-        image[row][col]=newColor;
-        int n = image.length;
-        int m = image[0].length;
+  void dfs(int row,int col,int[][] ans,int iniColor,int newColor,int delRow[], int delCol[]){
+        ans[row][col] = newColor;
+        int n = ans.length;
+        int m = ans[0].length;
         for(int i = 0;i<4;i++) {
             int nrow = row + delRow[i]; 
             int ncol = col + delCol[i]; 
             // check for valid coordinate 
             // then check for same initial color and unvisited pixel
             if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
-            image[nrow][ncol] == iniColor && image[nrow][ncol]!=newColor) {
-                dfs(nrow, ncol, image,iniColor, newColor, delRow, delCol); 
+            ans[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor) {
+                dfs(nrow, ncol,ans,iniColor, newColor, delRow, delCol); 
             }
         }
         
